220decffb74e0e29d479bcdc5fd52c5d859e079f
Sir Tapos <qpdrjorge@tip.edu.ph>
2025-11-09 12:38:08 +0800

    Delete oop_javafx directory

--- oop_javafx/Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.oop_javafx;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(firstPgApplication.class, args);
-    }
-}
